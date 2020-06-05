3aad5575bae961db519da301e1bdde2020dbbf45
Mikhail Shuvarov <shyvarov@gmail.com>
2020-06-05 14:40:05 +0300

    feat: exercises completed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,35 @@
 'use strict';
+
+
+let num = 266219;
+
+//Вариант с массивом
+let result = 1,
+ numArr = String(num).split('');
+
+for(let i = 0; i < numArr.length; i++) {
+  result *= numArr[i];
+}
+
+console.log(result);
+
+result **= 3;
+
+console.log(result);
+
+
+//Вариант без массива
+let remain;
+result = 1;
+
+  while (num) {
+    remain = num % 10;
+    num = (num - remain) / 10;
+    result  *= remain;
+  }
+
+console.log(result);
+
+result **= 3;
+
+console.log(result);
