efe4065a50c08cfc4e9a68a90117d5fcf1d0ed1a
Mikhail Shuvarov <shyvarov@gmail.com>
2020-06-09 13:13:28 +0300

    fix: add a new condition

--- js/script.js
+++ js/script.js
@@ -4,11 +4,15 @@ function getString(data) {
   if (typeof(data) !== 'string') {
     return (data + ' - не строка. Введите строку.');
   } else {
-      if(data.length > 30) {
+    if (data !== '' && data !== ' '){
+      if (data.length > 30) {
         return data.trim().slice(0, 30) + '...';
       } else {
         return data.trim();
       }
+    } else {
+      return 'Введите не пустую строку';
+    }
   }
 }
 
@@ -17,6 +21,8 @@ console.log(getString(true));
 console.log(getString(null));
 console.log(getString(undefined));
 
+console.log(getString(' '));
+console.log(getString(''));
 console.log(getString('   Далеко-далеко  '));
 console.log(getString('Далеко-далеко'));
 console.log(getString('Далеко-далеко, за словесными горами в стране гласных и согласных живут рыбные тексты.'));
