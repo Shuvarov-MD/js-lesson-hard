32ff9a67d865b999185796c255fe48839d859ae0
Mikhail Shuvarov <shyvarov@gmail.com>
2020-06-12 20:12:03 +0300

    feat: lesson07 complited

--- js/script.js
+++ js/script.js
@@ -1,36 +1,14 @@
 'use strict';
 
+let week = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'];
+let today = new Date();
 
-//Задание 1
-let isNumber = function(n) {
-  return !isNaN(parseFloat(n)) && isFinite(n);
-};
-
-let arr = [];
-
-for (let i = 0; i < 7; i++) {
-  do {
-    arr[i] = prompt('Введите многозначное число:', '').trim();
-  } while (!isNumber(arr[i]) || arr[i].length < 2);
-}
-
-console.log(arr);
-
-for (let i = 0; i < 7; i++) {
-  if (arr[i][0] === '2') {
-    console.log('Числа, начинающиеся с цифры 2: ' + arr[i]);
-  } else if (arr[i][0] === '4') {
-    console.log('Числа, начинающиеся с цифры 4: ' + arr[i]);
+week.forEach(function(item, index) {
+  if (index === today.getDay()) {
+    document.write('<strong>' + item + '</strong><br>');
+  } else if (index === 0 || index === 6) {
+    document.write('<em>' + item + '</em><br>');
+  } else {
+    document.write(item + '<br>');
   }
-}
-
-
-//Задание 2
-outer: for (let i = 1; i <= 100; i++) {
-  for (let j = 2; j < i; j++) {
-    if (i % j === 0) {
-      continue outer;
-    }
-  }
-  console.log('Делители этого числа: ' + 1 + ' и ' + i);
-}
+});
