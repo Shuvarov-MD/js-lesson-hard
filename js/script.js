de16910944ee86c5881a6445fcde610e74bbe55d
Mikhail Shuvarov <shyvarov@gmail.com>
2020-06-22 16:44:44 +0300

    feat: lesson13 complited

--- js/script.js
+++ js/script.js
@@ -1,48 +1,46 @@
 'use strict';
 
-//Функция для вывода полной записи времени
-function showTimeFull() {
-  let date = new Date(),
-    fullDate = document.querySelector('#full-date'),
-    days = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'],
-    months = ['января', 'февраля', 'марта', 'апреля', 'мая', 'июня', 'июля', 'августа', 'сентября', 'октября', 'ноября', 'декабря'],
-    timeFull = {
-      dayFull: days[date.getDay()],
-      dateFull: date.getDate(),
-      monthFull: months[date.getMonth()],
-      yearFull: date.getFullYear(),
-      hoursFull: date.getHours(),
-      minutesFull: date.getMinutes(),
-      secondsFull: date.getSeconds()
-    },
-    hour = (timeFull.hoursFull % 10 === 2 || timeFull.hoursFull % 10 === 3 || timeFull.hoursFull % 10 === 4) ? ' часа ' : (timeFull.hoursFull % 10 === 1 && timeFull.hoursFull !== 11) ? ' час ' : ' часов ',
-    minute = (timeFull.minutesFull % 10 === 2 || timeFull.minutesFull % 10 === 3 || timeFull.minutesFull % 10 === 4) ? ' минуты ' : (timeFull.minutesFull % 10 === 1 && timeFull.minutesFull !== 11) ? ' минута ' : ' минут ',
-    second = (timeFull.secondsFull % 10 === 2 || timeFull.secondsFull % 10 === 3 || timeFull.secondsFull % 10 === 4) ? ' секунды' : (timeFull.secondsFull % 10 === 1 && timeFull.secondsFull !== 11) ? ' секунда' : ' секунд';
+const color = document.getElementById('color'),
+  change = document.getElementById('change');
 
-   fullDate.innerHTML = 'Сегодня ' + timeFull.dayFull +', '+ timeFull.dateFull + ' ' + timeFull.monthFull + ' ' + timeFull.yearFull + ' года, ' + timeFull.hoursFull + hour + timeFull.minutesFull + minute + timeFull.secondsFull + second;
-}
+  document.body.style.backgroundColor = '#008d3b';
+  color.style.textAlign = 'center';
+  color.style.color = '#FFF';
+  change.style.display = 'block';
+  change.style.margin = '0 auto';
 
 
-//Функция для вывода короткой записи времени
-function showTimeShort() {
-  let date = new Date(),
-    shortDate = document.querySelector('#short-date'),
-    timeShort = {
-      dateFull: date.getDate(),
-      monthFull: date.getMonth() + 1,
-      yearFull: date.getFullYear(),
-      hoursFull: date.getHours(),
-      minutesFull: date.getMinutes(),
-      secondsFull: date.getSeconds()
-    };
+//Функция получения 16-ричного числа
+let getNumber = function() {
+  let redNumber = Math.floor(Math.random() * 256),
+    greenNumber = Math.floor(Math.random() * 256),
+    blueNumber = Math.floor(Math.random() * 256);
 
-  for (let key in timeShort) {
-    if (timeShort[key] < 10) {
-      timeShort[key] = '0' + timeShort[key];
-    }
+  let redColor = redNumber.toString(16);
+  if (redColor.length < 2) {
+    redColor = '0' + redColor;
   }
-  shortDate.innerHTML = timeShort.dateFull + '.' + timeShort.monthFull + '.' + timeShort.yearFull + ' - ' + timeShort.hoursFull + ':' + timeShort.minutesFull + ':' + timeShort.secondsFull;
-}
 
-setInterval(showTimeFull, 1000);
-setInterval(showTimeShort, 1000);
+  let greenColor = greenNumber.toString(16);
+  if (greenColor.length < 2) {
+    greenColor = '0' + greenColor;
+  }
+
+  let blueColor = blueNumber.toString(16);
+  if (blueColor.length < 2) {
+    blueColor = '0' + blueColor;
+  }
+
+  return redColor + greenColor + blueColor;
+};
+
+
+//Функция изменения цвета и текста
+let render = function() {
+  let colorNumber = getNumber();
+  document.body.style.backgroundColor = '#' + colorNumber;
+  color.textContent = '#' + colorNumber;
+};
+
+
+change.addEventListener('click', render);
