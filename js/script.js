e63089b1eaece0d8c0dc9bb6a72dbce0b166d1ec
Mikhail Shuvarov <shyvarov@gmail.com>
2020-06-29 19:21:58 +0300

    feat: lesson17-hard completed

--- js/script.js
+++ js/script.js
@@ -1,46 +1,61 @@
 'use strict';
 
-const color = document.getElementById('color'),
-  change = document.getElementById('change');
+document.body.style.backgroundImage = "url('../img/sky.jpg')";
+document.body.style.backgroundRepeat = "no-repeat";
+document.body.style.backgroundSize = "cover";
 
-  document.body.style.backgroundColor = '#008d3b';
-  color.style.textAlign = 'center';
-  color.style.color = '#FFF';
-  change.style.display = 'block';
-  change.style.margin = '0 auto';
+const airplane = document.querySelector('#airplane');
+
+let start = document.createElement('button'),
+  reset = document.createElement('button');
 
 
-//Функция получения 16-ричного числа
-let getNumber = function() {
-  let redNumber = Math.floor(Math.random() * 256),
-    greenNumber = Math.floor(Math.random() * 256),
-    blueNumber = Math.floor(Math.random() * 256);
+airplane.style.position = 'relative';
 
-  let redColor = redNumber.toString(16);
-  if (redColor.length < 2) {
-    redColor = '0' + redColor;
+start.classList.add('start');
+start.textContent = 'Start';
+start.style.display = 'block';
+
+reset.classList.add('reset');
+reset.textContent = 'Reset';
+reset.style.display = 'none';
+
+airplane.before(start);
+start.after(reset);
+
+let count = 0;
+
+let flyInterval;
+let pause = false;
+
+const flyAirplane = () => {
+  flyInterval = requestAnimationFrame(flyAirplane);
+  count++;
+  console.log(count);
+  airplane.style.top = count + 'px';
+  airplane.style.left = (count * 3) + 'px';
+  if(count > 500) {
+    cancelAnimationFrame(flyInterval);
   }
-
-  let greenColor = greenNumber.toString(16);
-  if (greenColor.length < 2) {
-    greenColor = '0' + greenColor;
-  }
-
-  let blueColor = blueNumber.toString(16);
-  if (blueColor.length < 2) {
-    blueColor = '0' + blueColor;
-  }
-
-  return redColor + greenColor + blueColor;
 };
 
 
-//Функция изменения цвета и текста
-let render = function() {
-  let colorNumber = getNumber();
-  document.body.style.backgroundColor = '#' + colorNumber;
-  color.textContent = '#' + colorNumber;
-};
+start.addEventListener('click', () => {
+  if (!pause) {
+    flyInterval = requestAnimationFrame(flyAirplane);
+    reset.style.display = 'block';
+    pause = true;
+  } else {
+    cancelAnimationFrame(flyInterval);
+    pause = false;
+  }
 
+});
 
-change.addEventListener('click', render);
+reset.addEventListener('click', () => {
+  cancelAnimationFrame(flyInterval);
+  count = 0;
+  airplane.style.top = 0;
+  airplane.style.left = 0;
+  reset.style.display = 'none';
+});
